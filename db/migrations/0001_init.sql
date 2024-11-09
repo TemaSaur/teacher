aa881ac4f0c9fcb736f67b46a2072960c665fd7c
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2024-11-09 14:25:49 +0500

    initialize/test mysql connection

---
+++ db/migrations/0001_init.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+-- migrate:down
+
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from sqlalchemy import create_engine
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from repository.user import Querier
+import os
+
+
+load_dotenv(".env")
+
+engine = create_engine(os.getenv("DATABASE_URL")
+                       .replace("mysql://", "mysql+pymysql://"))
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return "hello"
+
+
+@app.get("/data")
+def data():
+    with engine.connect() as conn:
+        result = Querier(conn).get_all()
+        return list(result)
 
