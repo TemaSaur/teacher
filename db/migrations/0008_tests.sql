467480905227d8e10b274de8254d94be5b13920e
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2024-11-20 22:44:36 +0500

    move test data to the tests table

--- db/migrations/0008_tests.sql
+++ db/migrations/0008_tests.sql
@@ -1,24 +1,15 @@
 -- migrate:up
 CREATE TABLE tests (
-	id      INT     NOT NULL AUTO_INCREMENT,
-	name    TEXT    NOT NULL,
-	class   TINYINT NOT NULL,
-	quarter TINYINT NOT NULL,
-	topic   TEXT    NOT NULL,
+	id        INT     NOT NULL AUTO_INCREMENT,
+	name      TEXT    NOT NULL,
+	class     TINYINT NOT NULL,
+	quarter   TINYINT NOT NULL,
+	topic     TEXT    NOT NULL,
+	test_data JSON    NOT NULL,
 
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE questions (
-	test_id   INT  NOT NULL,
-	test_data JSON NOT NULL,
-
-	PRIMARY KEY(test_id),
-	FOREIGN KEY(test_id) REFERENCES tests(id)
-);
-
 -- migrate:down
-DROP TABLE questions;
-
 DROP TABLE tests;
 
